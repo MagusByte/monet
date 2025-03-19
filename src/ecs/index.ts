71300b7aa3663c8bd835a84ddcd4d0fea4b6cb48
Wouter Lindenhof <wouterlindenhof@hey.com>
2025-03-19 14:40:59 +0100

    move files around

---
+++ src/ecs/index.ts
@@ -0,0 +1,6 @@
+export * from './Component';
+export * from './Entity';
+export * from './ISystem';
+
+export * from './visit/visit-children';
+export * from './visit/visit-parent';
---
+++ src/shapes/index.ts
@@ -0,0 +1,4 @@
+export * from './Circle2d';
+export * from './Line2d';
+export * from './Quad2d';
+export * from './Vec2d';
