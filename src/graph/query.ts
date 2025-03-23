d91483300e0134bb1cd1bb47a5fac0e5b598fdfb
Wouter Lindenhof <wouterlindenhof@hey.com>
2025-03-23 21:28:41 +0100

    refactor: Convert QueryNode to a class and implement IQueryNode interface

--- src/graph/query.ts
+++ src/graph/query.ts
@@ -1,28 +1,37 @@
 import { GraphNode } from './GraphNode';
 
-export interface QueryNode<T> {
+export interface IQueryNode<T> {
   isAncestorOf(node: GraphNode<T>): boolean;
   getChildren(): Generator<GraphNode<T>>;
 }
 
-export function query<T>(subject: GraphNode<T>): QueryNode<T> {
-  return {
-    isAncestorOf(node: GraphNode<T>): boolean {
-      let current = node.parent;
-      while (current) {
-        if (current === subject) {
-          return true;
-        }
-        current = current.parent;
+export class QueryNode<T> implements IQueryNode<T> {
+  private subject: GraphNode<T>;
+
+  constructor(subject: GraphNode<T>) {
+    this.subject = subject;
+  }
+
+  isAncestorOf(node: GraphNode<T>): boolean {
+    let current = node.parent;
+    while (current) {
+      if (current === this.subject) {
+        return true;
       }
-      return false;
-    },
-    *getChildren(): Generator<GraphNode<T>> {
-      let current = subject.firstChild;
-      while (current) {
-        yield current;
-        current = current.nextSibling;
-      }
-    },
-  };
+      current = current.parent;
+    }
+    return false;
+  }
+
+  *getChildren(): Generator<GraphNode<T>> {
+    let current = this.subject.firstChild;
+    while (current) {
+      yield current;
+      current = current.nextSibling;
+    }
+  }
+}
+
+export function query<T>(subject: GraphNode<T>): IQueryNode<T> {
+  return new QueryNode(subject);
 }
