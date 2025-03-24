ee46745d2b1a763cb2db1dc0d3d443c5b96cf3d6
Wouter Lindenhof <wouterlindenhof@hey.com>
2025-03-24 11:16:58 +0100

    test: Add initial tests for SystemBase class

---
+++ src/ecs/SystemBase.test.ts
@@ -0,0 +1,10 @@
+import { describe, test, expect } from 'vitest';
+import { SystemBase } from './SystemBase';
+
+describe('SystemBase', () => {
+  test('should be defined', () => {
+    expect(SystemBase).toBeDefined();
+  });
+
+  // Add more tests here as methods or properties are added to SystemBase
+});
